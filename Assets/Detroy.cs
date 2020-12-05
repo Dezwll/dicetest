ed5b8f7201b099507adad3b1f798e40d9e5a9ea7
Alex K <alexandr.kibo@gmail.com>
2020-12-06 00:09:40 +0300

    main
    init

---
+++ Assets/Detroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detroy : MonoBehaviour
+{
+    private void OnColliderStay2D(Collider other)
+    {
+        if (other.tag == "Place1" && !DragControl.isDrag)
+        {
+            Destroy(gameObject, 1);
+        }
+    }
+}
---
+++ Assets/DragControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragControl : MonoBehaviour
+{
+    public GameObject itemOne, itemTwo, itemThree, itemFour;
+    Vector2 itemOneInitial, itemTwoInitial, itemThreeInitial, itemFourInitial;
+    public Canvas canvas;
+    public static bool isDrag;
+
+    void Start()
+    {
+        itemOneInitial = itemOne.transform.position;
+        itemTwoInitial = itemTwo.transform.position;
+        itemThreeInitial = itemThree.transform.position;
+        itemFourInitial = itemFour.transform.position;
+        isDrag = false;
+    }
+
+    public void DragitemOne()
+    {
+        itemOne.transform.position = Input.mousePosition;
+    }
+
+    public void DragitemTwo()
+    {
+        itemTwo.transform.position = Input.mousePosition;
+    }
+
+    public void DragitemThree()
+    {
+        itemThree.transform.position = Input.mousePosition;
+    }
+
+    public void DragitemFour()
+    {
+        itemFour.transform.position = Input.mousePosition;
+    }
+
+    private void OnMouseDown()
+    {
+        isDrag = true;
+        Debug.Log("true");
+    }
+
+    private void OnMouseUp()
+    {
+        isDrag = false;
+        Debug.Log("false");
+    }
+}
